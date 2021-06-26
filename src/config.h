7e612e9f2f9abb9d03f3bb624b7b9733d134284d
James Ong Rui Ming <jamesong054@gmail.com>
2021-06-26 21:57:48 +0800

    Banned

--- src/config.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "file_handler.h"
-
-struct ConfigDetails {
-    char config_name[255];
-    char config_value[255];
-};
-
-void update_config(char config_name[255], char config_value[255]);
